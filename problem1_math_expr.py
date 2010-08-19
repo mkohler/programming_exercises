78860518ac12165e993ffc4ae0bad0afe3fa0498
Mark Kohler <kohler@netrino.com>
2010-08-19 16:02:11 -0400

    problem1: Finished first draft.

--- problem1_math_expr.py
+++ problem1_math_expr.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python2.6
 from __future__ import division
 import operator
 import unittest
@@ -8,72 +9,33 @@ class NotAnOperatorError(ValueError):
 
 
 class Node(object):
-    operator_dict = { '+': operator.add,
-                      '-': operator.sub,
-                      '*': operator.mul,
-                      '/': operator.truediv }
+    operator_dict = {'+': operator.add,
+                     '-': operator.sub,
+                     '*': operator.mul,
+                     '/': operator.truediv}
 
     def __init__(self, symbol):
         self.symbol = symbol
-
         self.children = []
 
-        #if oper not in ('+', '-', '*', '/'):
-        #    raise NotAnOperatorError('"%s" is not a supported operator' % oper)
-        #self.oper = oper
-
     def add_node(self, symbol):
-        self.children.append(Node(symbol))
+        child_node = Node(symbol)
+        self.children.append(child_node)
+        return child_node
+
+    def add_nodes(self, symbol_list):
+        return [self.add_node(s) for s in symbol_list]
 
     def calculate(self):
         if not self.children:
             return self.symbol
 
-        values = [ x.calculate() for x in self.children ]
+        values = [x.calculate() for x in self.children]
 
         return reduce(self.operator_dict[self.symbol], values)
 
-#        for node in nodes:
-#            if node is a number:
-#                return number
-#            else:
-#                calculate(node)
-
-
-
-
-    # Traverse the tree, out-of-order, in order to print on a terminal,
-    # sideways.
-#    def __str__(self):
-#        return self.str_r(self.root, 0)
-#
-#    def str_r(self, node, level):
-#        if node is None:
-#            return ''
-#        return (self.str_r(node._right, level+1)
-#                + "%s%s:%s\n" % (6 * level * ' ', node.key, node.data)
-#                + self.str_r(node._left, level+1))
-
-
-class Tests(unittest.TestCase):
-    def test_good_operator(self):
-        root = Node('+')
-
-    def xtest_bad_operator(self):
-        self.assertRaises(NotAnOperatorError, Node, 'x')
-
-    def test_add_leaf(self):
-        root = Node('+')
-        root.add_node(2.3)
-        root.add_node(2.3)
-
 
 class TestCalculate(unittest.TestCase):
-    def test_calc_add_basic(self):
-        root = Node('+')
-        root.add_node(1)
-        root.add_node(1)
-        self.assertEqual(root.calculate(), 2)
 
     def test_calc_add_basic(self):
         root = Node('+')
@@ -82,11 +44,27 @@ class TestCalculate(unittest.TestCase):
         root.add_node(1)
         self.assertEqual(root.calculate(), 3)
 
-    def test_calc_sub_basic(self):
-        root = Node('-')
-        root.add_node(5.0)
-        root.add_node(0.5)
-        self.assertEqual(root.calculate(), 4.5)
+    def test_depth2(self):
+        root = Node('*')
+        node = root.add_node('+')
+        node.add_node(2)
+        node.add_node(3)
+
+        node = root.add_node('-')
+        node.add_node(10)
+        node.add_node(1)
+
+        self.assertEqual(root.calculate(), 45.0)
+
+    def test_unbalanced_tree(self):
+        root = Node('+')
+        depth1_nodes = root.add_nodes('/+')
+        depth1_nodes[0].add_nodes((45, 15))
+        add_children = depth1_nodes[1].add_nodes('---')
+        add_children[0].add_nodes((100, 10, 1))
+        add_children[1].add_nodes((200, 20, 2))
+        add_children[2].add_nodes((300, 30, 3))
+        self.assertEqual(root.calculate(), 537)
 
 
 if __name__ == '__main__':
