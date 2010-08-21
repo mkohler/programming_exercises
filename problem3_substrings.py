cc0783c8704ff7f260e76af711ebd48f05cf696b
Mark Kohler <kohler@netrino.com>
2010-08-20 23:54:41 -0400

    problem3: Finished adding comments and tests.

--- problem3_substrings.py
+++ problem3_substrings.py
@@ -27,7 +27,7 @@ import unittest
 
 def longest_substring(str1, str2):
 
-    # Use the shorter string as the "horizontal" or inner loop.
+    # To save memory, use the shorter string as the "horizontal" or inner loop.
     if len(str1) < len(str2):
         h_str = str1
         v_str = str2
@@ -35,7 +35,8 @@ def longest_substring(str1, str2):
         h_str = str2
         v_str = str1
 
-    # Initialize state variables.
+    # Initialize state variables. Use defaultdicts so that we don't have
+    # to store all of the zero values in the dynamic programming matrix.
     prev_row = collections.defaultdict(int)
     row = collections.defaultdict(int)
     longest_strings = set()
@@ -46,40 +47,66 @@ def longest_substring(str1, str2):
     for v_char in v_str:
         for i, h_char in enumerate(h_str):
 
-            # If the characters match, 
+            # If the characters don't match, do nothing.
+            # If they do, update the state variables.
             if h_char == v_char:
+
+                # If there's a match, then look back to the previous
+                # row and column to see how many previous characters
+                # matched. Add one to get the value for this location.
                 common_str_len = prev_row[i-1] + 1
                 row[i] = common_str_len
+
+                # If this common substring isn't one of the longest
+                # we've seen, continue.
+                if common_str_len < max_length_seen:
+                    continue
+
+                # Extact the common substring from the original string
+                # by looking back from our current location, i, by the
+                # length of the string.
                 common_substr = h_str[i - common_str_len + 1:(i + 1)]
 
-                if row[i] == max_length_seen:
+                # Either add this string to the set of equal-length strings,
+                # or if this one breaks the record, start a new set.
+                if common_str_len == max_length_seen:
                     longest_strings.add(common_substr)
                 elif row[i] >= max_length_seen:
-                    max_length_seen = row[i]
+                    max_length_seen = common_str_len
                     longest_strings = set([common_substr])
 
-                #print 'row[%s]: %s' % (i, row[i])
+        # After processing each row, discard the oldest row.
         prev_row = row
         row = collections.defaultdict(int)
 
-    return tuple(longest_strings)
+    return list(longest_strings)
 
 
 class TestCase(unittest.TestCase):
 
-    def test_something(self):
-        longest_substring('xydxyaa', 'abcdxyz')
-        longest_substring('aaaaaarackspacexxxxxx', 'abrackspacezzz')
-        longest_substring('aaaaaaaaa', 'aaaaaaaaaaaaaaaaaaaaaaaaaa')
+    def test_some_strings(self):
+        self.assertEqual(['dxy'], longest_substring('xydxyaa', 'abcdxyz'))
+        self.assertEqual(['substring'],
+                         longest_substring('aaaaaasubstringxxxxxx',
+                                           'absubstringzzz'))
+
+        self.assertEqual(['shorter'],
+                         longest_substring('shorter', 'shorterlonger'))
+        self.assertEqual(['shorter'],
+                         longest_substring('shorter', 'longershorter'))
 
     def test_multiple_same_length(self):
-        longest_substring('xxx123yyyy456zzz', '789zzz012xxx345yyy')
+        substrs = longest_substring('xxx123yyyy456zzz', '789zzz012xxx345yyy')
+        self.assertEqual(3, len(substrs))
+        self.assert_('xxx' in substrs)
+        self.assert_('yyy' in substrs)
+        self.assert_('zzz' in substrs)
 
     def test_no_common_substrings(self):
-        longest_substring('123456', 'abcdef')
+        self.assertEqual([], longest_substring('123456', 'abcdef'))
 
     def test_empty_string(self):
-        longest_substring('rackspace', '')
+        self.assertEqual([], longest_substring('somestring', ''))
 
 
 if __name__ == '__main__':
